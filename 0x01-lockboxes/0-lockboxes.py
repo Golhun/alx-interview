f28ebc49e96847b3e03652e9069417b3bc4da6a0
Gerald Adu-Broni <geraldduah1@gmail.com>
2024-07-31 21:42:11 +0000

    updated script to use queue implementation

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,39 @@
 #!/usr/bin/python3
+"""This module provides a solution to the lockbox,
+problem using a Queue implementation."""
+from collections import deque
+
+
+class Queue:
+    """A class implementation of a queue."""
+
+    def __init__(self):
+        """The constructor method of the class."""
+        self.items = deque()
+
+    def enqueue(self, item):
+        """This method enqueues items to the queue."""
+        self.items.append(item)
+
+    def dequeue(self):
+        """This method dequeues items from the queue."""
+        if not self.is_empty():
+            return self.items.popleft()
+        else:
+            raise IndexError("Dequeue from empty queue")
+
+    def is_empty(self):
+        """This method checks if the queue is empty."""
+        return len(self.items) == 0
+
+    @property
+    def size(self):
+        """This getter method returns the size of the queue."""
+        return len(self.items)
+
+    def __str__(self):
+        """This returns the string representation of the queue."""
+        return ", ".join([f"{x}" for x in self.items])
 
 
 def canUnlockAll(boxes):
@@ -14,25 +49,24 @@ def canUnlockAll(boxes):
     Returns:
         bool: True if all boxes can be unlocked, False otherwise.
     """
+    if not boxes:
+        return False
 
-    # Initialize the set to keep track of visited boxes
-    visited = set()
+    num_boxes = len(boxes)
+    visited = [False] * num_boxes
+    visited[0] = True
 
-    # Initialize the stack for Depth-First Search (DFS) with the first box
-    stack = [0]
+    queue = Queue()
+    queue.enqueue(0)
 
-    while stack:
-        # Get the current box from the stack
-        box = stack.pop()
+    while queue.size:
+        try:
+            current_box = queue.dequeue()
+            for key in boxes[current_box]:
+                if 0 <= key < num_boxes and not visited[key]:
+                    visited[key] = True
+                    queue.enqueue(key)
+        except IndexError:
+            return False
 
-        if box not in visited:
-            # Mark the current box as visited
-            visited.add(box)
-
-            # Add all keys in the current box to the stack,
-            # if they lead to unvisited boxes
-            for key in boxes[box]:
-                if key not in visited:
-                    stack.append(key)
-
-    return len(visited) == len(boxes)
+    return all(visited)
